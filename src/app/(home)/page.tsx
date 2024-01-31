20737b33aea9a3cc370f94f94d929fa732350755
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-01-30 22:11:59 -0300

    add component header

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+export default function Home() {
+  return (
+    <main 
+    className="flex min-h-screen flex-col p-1"
+    >
+      <Header />
+    </main>
+  );
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+
+
+export function Header() {
+  return (
+    <Card>
+      <CardContent className="p-5 flex justify-between items-center">
+        <Image
+          alt="logo"
+          src='/logo.png'
+          width={120}
+          height={22}
+        />
+        <Button variant='outline' size='icon'>
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
