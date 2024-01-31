be64faf0025a086ff83bded180e2e49fbb7ae585
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-01-31 20:27:19 -0300

    add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,18 @@
+import { format, } from 'date-fns'
+
 import { Header } from "../components/Header";
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
   return (
-    <main 
-    className="flex min-h-screen flex-col p-1"
+    <main
+      className="flex min-h-screen flex-col p-1"
     >
       <Header />
+      <div className='p-5'>
+        <h2 className='text-xl font-bold'>Òla, Vinicius!</h2>
+        <p className='capitalize text-sm'>{format(new Date(), "EE, dd 'de'  MMMM", { locale: ptBR })}</p>
+      </div>
     </main>
   );
 }
