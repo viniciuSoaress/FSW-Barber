2e636384ae26af0c9c3f49b4ea3e62657227b0a8
viniciszs <114860862+viniciszs@users.noreply.github.com>
2024-02-01 20:22:11 -0300

    add BarberShopItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,23 @@
 import { format, } from 'date-fns'
+import prisma from '../lib/prisma';
 
 import { Header } from "../components/Header";
 import { ptBR } from 'date-fns/locale';
 import { SearchHome } from '../components/SearchHome';
 import { BookingItem } from '../components/BookingItem';
+import { Barbershop } from '@prisma/client';
+import { BarberShopItem } from '../components/BarberShopItem';
 
-export default function Home() {
+export default async function Home() {
+
+  const barbersShops = await prisma.barbershop.findMany()
+
   return (
     <main
       className="flex min-h-screen flex-col p-1 gap-2"
     >
       <Header />
-      
+
       <section className='p-5'>
         <h2 className='text-xl font-bold'>Òla, Vinicius!</h2>
         <p className='capitalize text-sm'>{format(new Date(), "EE, dd 'de'  MMMM", { locale: ptBR })}</p>
@@ -24,6 +30,15 @@ export default function Home() {
       <section className='px-5 mt-6 text-gray-400'>
         <h2 className='text-xs uppercase mb-3 font-bold'>Agendamento</h2>
         <BookingItem />
+      </section>
+
+      <section className='px-5 mt-6 text-gray-400'>
+        <h2 className='text-xs uppercase mb-3 font-bold'>Recomendados</h2>
+        <div className=' flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
+          {barbersShops.map((barberShop: Barbershop) => (
+            <BarberShopItem barberShop={barberShop} key={barberShop.id} />
+          ))}
+        </div>
       </section>
 
     </main>
---
+++ src/app/components/BarberShopItem.tsx
@@ -0,0 +1,37 @@
+import { Barbershop } from "@prisma/client";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { StarIcon } from "lucide-react";
+
+type BarberShopItemProps = {
+  barberShop: Barbershop
+}
+
+export function BarberShopItem({ barberShop }: BarberShopItemProps) {
+  return (
+    <Card className="max-w-[167px] min-w-[167px] rounded-2xl">
+      <CardContent className="p-1 flex flex-col gap-2">
+        <div className="relative w-full h-40">
+          <Badge variant='secondary' className="absolute top-2 opacity-90 left-2 z-50">
+            <StarIcon size={12} className="fill-primary"/>
+            <span >5,0</span>
+          </Badge>
+          <Image
+            src={barberShop.imageUrl}
+            alt={barberShop.name}
+            fill
+            className="h-[159px] rounded-xl object-cover"
+          />
+        </div>
+
+        <div className="p-3 pt-0">
+          <h2 className="font-bold  overflow-hidden text-ellipsis text-nowrap">{barberShop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barberShop.address}</p>
+          <Button variant='secondary' className="w-full">Reservar</Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
